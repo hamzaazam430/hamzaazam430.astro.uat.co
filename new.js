c251ea7bfcb11e7575df187fe64f62a61133a2dc
Hamza Azam <hamzaazam430@outlook.com>
2022-09-08 17:13:53 +0500

    making request in bg

---
+++ new.js
@@ -0,0 +1,9 @@
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://eanooxhmk6nzczju7oppecbxty0qlpkc.lambda-url.us-east-1.on.aws/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
