71e05f4683e5da6dbdf9f3f4246081ed9a101425
Hamza Azam <hamzaazam430@outlook.com>
2022-09-09 11:58:56 +0500

    modifed the api call feature

--- new.js
+++ new.js
@@ -1,9 +1,12 @@
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+function makeRequest(){
+  postMessage();
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
 
-fetch("https://eanooxhmk6nzczju7oppecbxty0qlpkc.lambda-url.us-east-1.on.aws/", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  fetch("https://eanooxhmk6nzczju7oppecbxty0qlpkc.lambda-url.us-east-1.on.aws/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
