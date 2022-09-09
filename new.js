3d009e400a39f4daa260b4571fa7ed030ba1518b
Hamza Azam <hamzaazam430@outlook.com>
2022-09-09 16:06:26 +0500

    Changes the API URL to the main function and display message

--- new.js
+++ new.js
@@ -1,11 +1,11 @@
 function makeRequest(){
-  postMessage("API CALLED");
+  postMessage("System Initiated...");
   var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
 
-  fetch("https://eanooxhmk6nzczju7oppecbxty0qlpkc.lambda-url.us-east-1.on.aws/", requestOptions)
+  fetch("https://flujp7e2qiftmsvzvvmcqobvga0zvynu.lambda-url.us-east-1.on.aws/", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
