9e2e40eef8894b74de81cb0ef05c69562cfc4ef7
hamzaazam430 <78200801+hamzaazam430@users.noreply.github.com>
2022-10-14 16:38:08 +0500

    Update and rename timer.js to request_dev.js

---
+++ request_dev.js
@@ -0,0 +1,14 @@
+function makeRequest(){
+  postMessage("System Initiated...");
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+makeRequest();
