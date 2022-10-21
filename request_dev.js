a820944a60e32c3ee593ee7782b57fcf92ddda38
Asimak47 <asim95239@gmail.com>
2022-10-21 11:22:59 +0500

    sixth commit, test

--- request_dev.js
+++ request_dev.js
@@ -5,15 +5,14 @@ function makeRequest(){
     redirect: 'follow'
   };
 
-  const r=fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
-  var data=r.json();
+fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
   console.log("work done!="+data);
-  console.log("response" + response)
-  console.log("result" + result)
+  //console.log("response" + response)
+  //console.log("result" + result)
 
-    //.then(response => response.text())
-    //.then(result => console.log(result))
-    //.catch(error => console.log('error', error));
 }
 
 makeRequest();
