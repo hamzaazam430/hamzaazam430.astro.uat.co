a5ab8b50b1c6a94c99d71ca10d8f95cae2ae918c
Asimak47 <asim95239@gmail.com>
2022-10-21 13:18:16 +0500

    16th commit

--- request_dev.js
+++ request_dev.js
@@ -1,20 +1,44 @@
+// function makeRequest(){
+//   postMessage("System Initiated...");
+//   var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+
+// fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
+// .then(response => {
+//   response.json();
+//   console.log("response receieved!");
+// })
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+//   console.log("work done!=");
+//   //console.log("response" + response)
+//   //console.log("result" + result)
+
+// }
+
+// makeRequest();
+
+
+// main.js
 function makeRequest(){
   postMessage("System Initiated...");
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+// GET request using fetch()
 
-fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
-.then(response => {
-  return response.json();
-  //console.log("response receieved!");
-})
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  console.log("work done!=");
-  //console.log("response" + response)
-  //console.log("result" + result)
+fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
+
+	// Converting received data to JSON
+	.then(response => {
+    response.json();
+    console.log("response receieved!");
+  })
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    console.log("work done!=");
+
+	// Display result
+	document.getElementById("Result").innerHTML = li;
 
 }
 
