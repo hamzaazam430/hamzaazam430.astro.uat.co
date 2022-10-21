6162c5f9c4cc3c3735e6608fdb128cb914011344
Asimak47 <asim95239@gmail.com>
2022-10-21 15:25:48 +0500

    25 commit

--- request_dev.js
+++ request_dev.js
@@ -32,12 +32,12 @@ fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
 	.then(response => {
     response.json();
     console.log("response receieved!");
-    alert ("Hello world!");
+    
   })
   //.then(_alert => window.alert("Hello world"))
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
- 
+  alert ("Hello world!");
   //console.log("response" + response)
   //console.log("result" + result)
 
