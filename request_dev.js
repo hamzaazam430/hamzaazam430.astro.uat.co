c48d42507562f1f38bd6dd67d8531acba4a57c7a
Asimak47 <asim95239@gmail.com>
2022-10-21 15:52:35 +0500

    30 commit

--- request_dev.js
+++ request_dev.js
@@ -1,33 +1,14 @@
-// function makeRequest(){
-//   postMessage("System Initiated...");
-//   var requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow'
-//   };
-
-// fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/", requestOptions)
-// .then(response => {
-//   response.json();
-//   console.log("response receieved!");
-// })
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-//   console.log("work done!=");
-//   //console.log("response" + response)
-//   //console.log("result" + result)
-
-// }
-
-// makeRequest();
-
-
 // main.js
 function makeRequest(){
   postMessage("System Initiated...");
 // GET request using fetch()
 
 fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
-
+function makeRequest() {
+  
+  console.log("message appeared");
+}
+setTimeout(makeRequest, 3000) 
 	// Converting received data to JSON
 	.then(response => {
     response.json();
