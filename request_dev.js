b0cf7c2da9af868cde2083bd4e6be0a6df3f2a17
Asimak47 <asim95239@gmail.com>
2022-10-21 16:22:50 +0500

    39 commit

--- request_dev.js
+++ request_dev.js
@@ -15,14 +15,9 @@ fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
   //.then(_alert => window.alert("Hello world"))
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+  console.log("It is not working");
  
-  function makeRequest() {
-  
-    console.log("message appeared");
-  }
-  setTimeout(()=>{
-    makeRequest();
-  },3000)
+ 
 }
 
 makeRequest();
