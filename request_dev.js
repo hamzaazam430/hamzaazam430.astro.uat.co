d52b8ef476c4fdd4e4fbd06c509bd87e1bbdd447
Asimak47 <asim95239@gmail.com>
2022-10-24 14:19:00 +0500

    commit60

--- request_dev.js
+++ request_dev.js
@@ -27,15 +27,16 @@ return new Promise(resolve => {
 makeRequest();
 
 
-// async function makeRequest() {
-//   let response = await fetch('https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/');
+async function makeRequest() {
+  let response = await fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
+  // let data = await response.text();
+  .then(response => {
+    response.json();
+    console.log("response receieved!");
 
-//   console.log(response.status); // 200
-//   console.log(response.statusText); // OK
-
-//   if (response.status === 200) {
-//       let data = await response.text();
-//       // handle data
-//   }
-// }
-// makeRequest();
+  })
+  //.then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  console.log(data);
+}
