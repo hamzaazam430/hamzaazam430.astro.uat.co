5ae819b28536a88e04b27f1be1b4d42655d503d9
Asimak47 <asim95239@gmail.com>
2022-10-24 12:58:11 +0500

    commit55

--- request_dev.js
+++ request_dev.js
@@ -1,11 +1,33 @@
-function makeRequest(){
-     postMessage("System Initiated...");
-const fetchPromise = fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/");
-const main = document.getElementById("Result");
-// Loading Placeholder
-main.innerHTML = "<p>Loading...";
-fetchPromise.then(response => {
-  return response.json();
-})
+// function makeRequest(){
+//   postMessage("System Initiated...");
+//   var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
 
+//   fetch("https://flujp7e2qiftmsvzvvmcqobvga0zvynu.lambda-url.us-east-1.on.aws/", requestOptions)
+//     .then(response => {
+//       response.json();
+//       console.log("response receieved!");
+  
+//     })
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// }
+
+// makeRequest();
+
+
+async function makeRequest() {
+  let response = await fetch('https://flujp7e2qiftmsvzvvmcqobvga0zvynu.lambda-url.us-east-1.on.aws/');
+
+  console.log(response.status); // 200
+  console.log(response.statusText); // OK
+
+  if (response.status === 200) {
+      let data = await response.text();
+      // handle data
+  }
 }
+makeRequest();
