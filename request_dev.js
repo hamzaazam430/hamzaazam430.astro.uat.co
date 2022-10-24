4543ecac90ad101176aa7dda3a146db59916f5e5
Asimak47 <asim95239@gmail.com>
2022-10-24 16:09:02 +0500

    commit82

--- request_dev.js
+++ request_dev.js
@@ -1,20 +1,18 @@
-function makeRequest(){
-    let requests =  fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/");
-
-    Promise.all(requests)
-      .then(responses => {
-        // all responses are resolved successfully
-        for(let response of responses) {
-          alert(`${response.url}: ${response.status}`); // shows 200 for every url
-        }
-    
-        return responses;
-      })
-      console.log("result" + responses);
-    //   // map array of responses into an array of response.json() to read their content
-    //   .then(responses => Promise.all)
-    //   // all JSON answers are parsed: "users" is the array of them
-    //   .then(user => alert(user.data));
+var makeRequest = function(){
+    postMessage("System Initiated...");
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    const Data = fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/",requestOptions)
+    .then(response => {
+          response.json();
+          console.log("response receieved!");
+      
+        })
+  console.log(Data);
+  console.log("hoja bhai");
+  widows.alert("Complete Processing");
+      }
   
-}
-makeRequest();
+  makeRequest();
