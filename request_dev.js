42a27470a471e48372caa08391206ac243cb4b62
Asimak47 <asim95239@gmail.com>
2022-10-24 12:06:14 +0500

    commit

--- request_dev.js
+++ request_dev.js
@@ -1,23 +1,48 @@
-// main.js
-function makeRequest(){
+// // main.js
+// function makeRequest(){
+//   postMessage("System Initiated...");
+// // GET request using fetch()
+
+// fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
+
+
+// 	// Converting received data to JSON
+// 	// .then(response => {
+//   //   response.json();
+//   //   console.log("response receieved!");
+//   //   //
+//   //   setTimeout(3000);
+//   // })
+//   // //.then(_alert => window.alert("Hello world"))
+//   // .then(result => console.log(result))
+//   // .catch(error => console.log('error', error));
+//   // console.log("It is not working");
+ 
+ 
+// }
+
+// makeRequest();
+
+function makeRequest(path) {
   postMessage("System Initiated...");
-// GET request using fetch()
-
-fetch("https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/")
-
-
-	// Converting received data to JSON
-	.then(response => {
-    response.json();
-    console.log("response receieved!");
-    setTimeout(3000);
-  })
-  //.then(_alert => window.alert("Hello world"))
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  console.log("It is not working");
- 
- 
+	return new Promise(function (resolve, reject) {
+		axios.get(path).then(
+			(response) => {
+				var result = response.data;
+				console.log('Processing Request');
+				resolve(result);
+			},
+				(error) => {
+				reject(error);
+			}
+		);
+	});
 }
 
-makeRequest();
+async function main() {
+	var result = await makeRequest('https://oaf7f6gd22mwknracpnhfkilqm0tpsii.lambda-url.us-east-1.on.aws/');
+	console.log(result.result);
+	console.log('Statement 2');
+}
+main();
+
